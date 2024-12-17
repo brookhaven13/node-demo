0a07b0cbc87566bf617f93fb16640dffa845e79d
Harley <yu.h.lin1991@gmail.com>
2024-12-17 23:25:18 +0800

    feat: 🎸 install mongoose & create tour model

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+
+mongoose.connect(DB)
+  .then((connect) => {
+    console.log('DB connection successful!', connect);
+  })
+  .catch((err) => {
+    console.log('DB connection failed!', err);
+  });
 
 
 const port = process.env.PORT || 3000;
